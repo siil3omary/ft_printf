07160ec2f231e28cd02bc4862aa6891fbef75d4c
SII.L3OMARY <L3OMARY@ICLOUD.COM>
2023-11-28 13:12:28 +0100

    done

---
+++ ft_printf.c
@@ -0,0 +1,6 @@
+#include "./includes/ft_printf.h"
+
+int    ft_printf(const char *, ...)
+{
+	
+}
---
+++ includes/ft_printf.h
@@ -0,0 +1,6 @@
+#if !defined(FT_PRINTF_H)
+#define FT_PRINTF_H
+
+int    ft_printf(const char *, ...);
+
+#endif // FT_PRINTF_H
